18fb2ffbd6766d2778e3311070c417c6f6abc3c0
Nate Merrill <merrill.nathaniel@epa.gov>
2015-11-09 15:22:58 -0500

    got it to work with json

--- flikrtry.py
+++ flikrtry.py
@@ -1,18 +1,30 @@
-#Python script to download Flikr data from API
-#Author: Nate Merrill
-#Started 11/9/2015
+##Python script to download Flikr data from API
+##Author: Nate Merrill
+##Started 11/9/2015
 
 import sys, os
 import flickrapi
+import json
 
+#logs into flikr API 
 api_key = u'ce2f76d42f363d6199b9fa0d55b6869f'
 api_secret = u'cf32baacaa9132e2'
+flickr = flickrapi.FlickrAPI(api_key, api_secret,format='parsed-json') #sets format for return
 
-flickr = flickrapi.FlickrAPI(api_key, api_secret)
+#uses API functions
 photos = flickr.photos.search(user_id='73509078@N00', per_page='100',has_geo='1')
 
-for child in photos.iter():
-    print child.tag, child.attrib
-    
+photos['photos']['photo'][1]['id']
 
-photocheck=flickr.photos.getInfo(photo_id='8560608555')
+photo2=photos['photos']['photo']
+
+#function for getting list of photos taht met API query
+def encrypt(photo2):
+    results=list()
+    for i in photo2:
+        results.append( (i['id'])
+    return results
+
+r=encrypt(photo2)
+                        
+print r
