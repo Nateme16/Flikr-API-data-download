54dd4b47bedf72342309d974ac89fad5c908fec5
Nate Merrill <merrill.nathaniel@epa.gov>
2015-11-10 16:37:00 -0500

    not it pulls all of the photos
    Added the bit to get photos after a date on the Cape

--- flikrtry.py
+++ flikrtry.py
@@ -2,9 +2,7 @@
 ##Author: Nate Merrill
 ##Started 11/9/2015
 
-import sys, os
 import flickrapi
-import json
 
 #logs into flikr API 
 api_key = u'ce2f76d42f363d6199b9fa0d55b6869f'
@@ -13,28 +11,42 @@ flickr = flickrapi.FlickrAPI(api_key, api_secret,format='parsed-json')
 #sets format for return
 
 #uses API functions
-photos = flickr.photos.search(user_id='73509078@N00', per_page='100',has_geo='1')
+photos = flickr.photos.search(user_id='73509078@N00', per_page='100',has_geo='1') #searches for 1 user
 
 photos['photos']['photo'][1]['id']
 
-photo2=photos['photos']['photo']
+photo2=photos['photos']['photo'] 
 
-#function for getting list of photos taht met API query
-def encrypt(photo2):
+#function for getting id list of photos that met API query
+def plist(photo2):
 	results=list()
 	for i in photo2:
 		results.append(i['id'])
 	return results
 
-r=encrypt(photo2)
+r=plist(photo2)
                         
 print r
 
-#get photos by woeid
+#get photos by woeid for Cape Cod
 
 capewo=flickr.places.find(query='Barnstable') #gets id for the Cape
 cape2=flickr.places.getInfo(woe_id='12588700') #gets place info
-
 cape3=flickr.photos.search(woe_id='12588700') #gets info all photos taken on Cape
 
+#flickr = flickrapi.FlickrAPI(api_key, api_secret,format='etree')
+
+#this retreives all photos on the cape after datemin
+flickr = flickrapi.FlickrAPI(api_key, api_secret,format='etree') #has to be in etree format for some reason
+
+datemin= '2015-10-01'
+def pcape(datemin):
+    results2=list()
+    for photo in flickr.walk(woe_id='12588700',min_taken_date=datemin, per_page=250):
+        results2.append(photo.get('title'))
+    return results2
+    
+r2=pcape(datemin)
+    
+    
 
